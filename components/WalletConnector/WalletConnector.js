7a27e76ff3a405519235d7b42efcda0514daf469
Ilian Petrov <Ilian.Petrov@digitall.com>
2022-04-08 19:37:18 +0300

    reset account button to get from dv

--- components/WalletConnector/WalletConnector.js
+++ components/WalletConnector/WalletConnector.js
@@ -1,14 +1,14 @@
-import Image from 'next/image';
-import { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { useTranslations } from 'next-intl';
-import { useWeb3React } from '@web3-react/core';
-import { useMutation,  gql } from '@apollo/client';
-import { injected } from '../../utils/connectors';
-import { setWalletId, acceptTerms, rejectTerms } from '../../redux/user';
-import BaseContentLayout from '../../components/BaseContentLayout/BaseContentLayout';
-import styles from './WalletConnector.module.css';
-import { useRouter } from 'next/router';
+import Image from "next/image";
+import { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useTranslations } from "next-intl";
+import { useWeb3React } from "@web3-react/core";
+import { useMutation, gql } from "@apollo/client";
+import { injected } from "../../utils/connectors";
+import { setWalletId, acceptTerms, rejectTerms } from "../../redux/user";
+import BaseContentLayout from "../../components/BaseContentLayout/BaseContentLayout";
+import styles from "./WalletConnector.module.css";
+import { useRouter } from "next/router";
 
 import DataVaultWebClient, {
   // AuthManager,
@@ -16,13 +16,16 @@ import DataVaultWebClient, {
 } from "@rsksmart/ipfs-cpinner-client";
 import AuthManager from "../../AuthManager";
 import truncateAccount from "@/utils/truncateAccount";
+import { Button } from "components";
+import { createDidFormat } from "@/utils/vcUtils";
+
+const serviceUrl = "https://data-vault.identity.rifos.org";
 
 function WalletConnector({ onNext }) {
-  const termsAccepted = useSelector((state) => state.user.termsAccepted);
+  const termsAccepted = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
-  const { active, account, library, connector, activate, deactivate } =
-    useWeb3React();
+  const { active, account, chainId, activate, deactivate } = useWeb3React();
 
   const t = useTranslations("onboarding");
 
@@ -35,7 +38,7 @@ function WalletConnector({ onNext }) {
       console.error(error);
     }
   }, []);
-  
+
   async function connect() {
     try {
       await activate(injected);
@@ -52,47 +55,10 @@ function WalletConnector({ onNext }) {
     }
   }
 
-  const onSubmit = async () => {
+  const onSubmit = () => {
     if (active && account) {
       dispatch(setWalletId(account));
 
-      const did = `did:ethr:rsk:${account}`;
-      console.log("did", did);
-
-      const encryptionManager =
-        await AsymmetricEncryptionManager.fromWeb3Provider(window.ethereum);
-
-      const dataVault = new DataVaultWebClient({
-        serviceUrl,
-        authManager: new AuthManager({
-          did,
-          serviceUrl,
-          personalSign: (data) =>
-            window.ethereum.request({
-              method: "personal_sign",
-              params: [data, account],
-            }),
-        }),
-        encryptionManager,
-      });
-
-      // const key = "MyKey";
-      // const content = "this is my content 3";
-
-      // // let info = await dataVault.getStorageInformation();
-
-      // // console.log("info", info);
-      // // const id = await dataVault.create({ key, content });
-      // // console.log("id", id);
-
-      // await dataVault.delete({
-      //   key,
-      //   id: "QmTwV6MzK3rnLgRL3XXbX4Uu4Xe6xf4ssb2mGu8i7AvSri",
-      // });
-
-      // let result = await dataVault.get({ did, key });
-
-      // console.log("result", result);
       onNext(); // .catch((err) => err);
       // createWallet().then(() =>
       //   createUser()
@@ -105,14 +71,66 @@ function WalletConnector({ onNext }) {
   const onChangeCheckbox = ({ target }) =>
     target.checked ? dispatch(acceptTerms()) : dispatch(rejectTerms());
 
+  const buttonIsActive = !active || !account || !termsAccepted;
+
+  const onCustonButtonClick = async () => {
+    let did = createDidFormat(account, chainId);
+
+    const encryptionManager =
+      await AsymmetricEncryptionManager.fromWeb3Provider(window.ethereum);
+
+    const dataVault = new DataVaultWebClient({
+      serviceUrl,
+      authManager: new AuthManager({
+        did,
+        serviceUrl,
+        personalSign: (data) =>
+          window.ethereum.request({
+            method: "personal_sign",
+            params: [data, account],
+          }),
+      }),
+      encryptionManager,
+    });
+
+    const key = "Store";
+
+    let result = await dataVault.get({ did, key });
+
+    console.log("result", result);
+    // const content = "this is my content 3";
+
+    // // let info = await dataVault.getStorageInformation();
+
+    // // console.log("info", info);
+    // // const id = await dataVault.create({ key, content });
+    // // console.log("id", id);
+
+    // await dataVault.delete({
+    //   key,
+    //   id: "QmTwV6MzK3rnLgRL3XXbX4Uu4Xe6xf4ssb2mGu8i7AvSri",
+    // });
+  };
+
+  const renderCustomButton = () => {
+    return (
+      <Button
+        disabled={buttonIsActive}
+        label="Reset Account"
+        onClick={onCustonButtonClick}
+      />
+    );
+  };
+
   return (
     <BaseContentLayout
       {...{
         submitButtonProps: {
           label: t("submitBtn"),
           onClick: onSubmit,
-          disabled: !active || !account || !termsAccepted,
+          disabled: buttonIsActive,
         },
+        renderCustomButton,
       }}
     >
       <div className={styles.wrapper}>
